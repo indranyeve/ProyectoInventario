d40ffaa5b10490534e7e8203378801b6e0faf7b3
indranyeve <indranyeve@gmail.com>
2021-03-11 00:52:27 -0400

    first commit

---
+++ MVC/connection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace MVC
+{
+    public class connection
+    {
+        public static SqlConnection getConnection()
+        {
+            SqlConnection ocon = new SqlConnection("Data Source=DESKTOP-3AO6C47;Initial Catalog=BaseDatosInventario;Integrated Security=True");
+            ocon.Open();
+            return ocon;
+        }
+    }
+}
---
+++ WebApiInventario/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApiInventario.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
