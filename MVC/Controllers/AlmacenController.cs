0dd2e79e5522388a0039d4f13718a12bd59123b7
indranyeve <indranyeve@gmail.com>
2021-03-12 11:22:56 -0400

    configuracion de proyecto lista y comiendo de crud almacen

---
+++ MVC/Controllers/AlmacenController.cs
@@ -0,0 +1,50 @@
+﻿using MVC.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class AlmacenController : Controller
+    {
+        
+        //Hosted web API REST Service base url  
+        string Baseurl = "https://localhost:44350/";
+        public async Task<ActionResult> Index()
+        {
+            List<mvcAlmacenModel> Info = new List<mvcAlmacenModel>();
+
+            using (var client = new HttpClient())
+            {
+                //Passing service base url  
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+                HttpResponseMessage Res = await client.GetAsync("api/Almacens/");
+
+                //Checking the response is successful or not which is sent using HttpClient  
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api   
+                    var Response = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the Employee list  
+                    Info = JsonConvert.DeserializeObject<List<mvcAlmacenModel>>(Response);
+
+                }
+                //returning the employee list to view  
+                return View(Info);
+            }
+        }
+    }
+}
---
+++ MVC/Models/mvcAlmacenModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC.Models
+{
+    public class mvcAlmacenModel
+    {
+        public int Id_Almacen { get; set; }
+        public string Descripcion { get; set; }
+        public string Estado { get; set; }
+    }
+}
